4d3295a1625c2d79bae50f9c4e2e9c1819b11ead
wubocong <wubocong123@163.com>
2016-12-04 16:04:57 +0800

    rollback

--- config/env/production.js
+++ config/env/production.js
@@ -1,5 +1,6 @@
 export default {
   session: 'secret-boilerplate-token',
   token: 'secret-jwt-token',
-  database: 'mongodb://wubocong:warrior123@localhost:27017/bds-prod',
+  database: 'mongodb://localhost:27017/bds-prod',
+  // database: 'mongodb://wubocong:warrior123@localhost:27017/bds-prod',
 }
