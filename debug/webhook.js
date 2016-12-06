7273d8b8f71f777572256e336b5aaaff74d0ce32
wubocong <wubocong123@163.com>
2016-12-07 01:51:43 +0800

    update dev ops

--- debug/webhook.js
+++ debug/webhook.js
@@ -3,7 +3,7 @@ const exec = require('child_process').exec
 
 const server = http.createServer((req, res) => {
   if (req.url.search(/webhook\/?$/i) > 0) {
-    exec('git pull --all && pm2 restart 2 && pm2 restart 4')
+    exec('git pull --all && pm2 restart webhook && pm2 restart index')
     res.writeHead(200)
     res.write('pig')
     res.end()
