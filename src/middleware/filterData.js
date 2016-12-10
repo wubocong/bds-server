35c5a00af158c41ec4afa339bf602b05e0144938
wubocong <wubocong123@163.com>
2016-12-10 15:24:04 +0800

    debug

--- src/middleware/filterData.js
+++ src/middleware/filterData.js
@@ -7,15 +7,16 @@ export async function filterData(ctx, next) {
   const method = ctx.method.toLowerCase()
   const methods = ['get', 'post', 'put', 'delete']
   if (method === 'put') {
-    ctx.request.fields = ctx.request.fields || JSON.parse(ctx.request.body)
-  }
-  if (!methods.includes(method)) {
-    ctx.throw(405, 'Method Not Allowed')
-    logger.error(url + 'filter method')
+    typeof ctx.request.body
+    ctx.request.fields = ctx.request.fields || ctx.request.body
   }
   if (method === 'get') {
     ctx.request.fields = ctx.request.files = null
     return next()
+  }
+  if (!methods.includes(method)) {
+    ctx.throw(405, 'Method Not Allowed')
+    logger.error(url + 'filter method')
   }
 
   const {defense, defenses, paper, user, users, comment, score, file, account, password, role, oldPassword, newPassword, studentIds, defenseIds, finalScore, remark} = ctx.request.fields
