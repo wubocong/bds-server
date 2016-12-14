a019c6a458454630b11f88bf0393a6bb664d8d9a
wubocong <wubocong123@163.com>
2016-12-15 02:35:35 +0800

    0.6.0-beta.3 released!

--- debug/webhook.js
+++ debug/webhook.js
@@ -3,15 +3,27 @@ const exec = require('child_process').exec
 
 const server = http.createServer((req, res) => {
   if (req.url.search(/reset\/?$/i)) {
-    exec('npm run restore')
-    res.writeHead(200)
-    res.write('dog')
-    res.end()
+    exec('npm run restore', (err, success) => {
+      if (err) {
+        res.writeHead(500)
+        res.write('fuck')
+      } else {
+        res.writeHead(200)
+        res.write('restore!')
+      }
+      res.end()
+    })
   } else if (req.url.search(/webhook\/?$/i) > 0) {
-    exec('git pull --all && pm2 restart index && pm2 restart webhook')
-    res.writeHead(200)
-    res.write('pig')
-    res.end()
+    exec('git pull --all && pm2 restart index && pm2 restart webhook', (err, success) => {
+      if (err) {
+        res.writeHead(500)
+        res.write('fuck')
+      } else {
+        res.writeHead(200)
+        res.write('hook!')
+      }
+      res.end()
+    })
   } else {
     res.writeHead(404)
     res.end()
